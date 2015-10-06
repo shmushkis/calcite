f4a5590f5da8e77f919f8907baae8b14f55b2034
Julian Hyde <jhyde@apache.org>
2015-10-06 13:54:23 -0700

    [CALCITE-879] COLLECT aggregate function
    Allow multisets and maps to be instances of RexLiteral
    
    Add RelBuilder.values

--- core/src/main/java/org/apache/calcite/tools/RelBuilder.java
+++ core/src/main/java/org/apache/calcite/tools/RelBuilder.java
@@ -65,6 +65,7 @@ import java.math.BigDecimal;
 import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Builder for relational expressions.
@@ -154,6 +155,11 @@ public class RelBuilder {
   /** Returns the type factory. */
   public RelDataTypeFactory getTypeFactory() {
     return cluster.getTypeFactory();
+  }
+
+  /** Returns the builder for {@link RexNode} expressions. */
+  public RexBuilder getRexBuilder() {
+    return cluster.getRexBuilder();
   }
 
   /** Creates a {@link ProtoRelBuilder}, a partially-created RelBuilder.
@@ -328,6 +334,18 @@ public class RelBuilder {
     return nodes.build();
   }
 
+  /** Creates an access to a field by name. */
+  public RexNode dot(RexNode node, String fieldName) {
+    final RexBuilder builder = cluster.getRexBuilder();
+    return builder.makeFieldAccess(node, fieldName, true);
+  }
+
+  /** Creates an access to a field by ordinal. */
+  public RexNode dot(RexNode node, int fieldOrdinal) {
+    final RexBuilder builder = cluster.getRexBuilder();
+    return builder.makeFieldAccess(node, fieldOrdinal);
+  }
+
   /** Creates a call to a scalar operator. */
   public RexNode call(SqlOperator operator, RexNode... operands) {
     final RexBuilder builder = cluster.getRexBuilder();
@@ -448,7 +466,7 @@ public class RelBuilder {
 
   /** Creates a group key. */
   public GroupKey groupKey(Iterable<? extends RexNode> nodes) {
-    return new GroupKeyImpl(ImmutableList.copyOf(nodes));
+    return new GroupKeyImpl(ImmutableList.copyOf(nodes), null);
   }
 
   /** Creates a group key of fields identified by ordinal. */
@@ -781,7 +799,7 @@ public class RelBuilder {
    *
    * <p>If there are zero rows, or if all values of a any column are
    * null, this method cannot deduce the type of columns. For these cases,
-   * call {@link #values(RelDataType, Iterable)}.
+   * call {@link #values(Iterable, RelDataType)}.
    *
    * @param fieldNames Field names
    * @param values Values
@@ -821,7 +839,7 @@ public class RelBuilder {
       rowTypeBuilder.add(name, type);
     }
     final RelDataType rowType = rowTypeBuilder.build();
-    return values(rowType, tupleList);
+    return values(tupleList, rowType);
   }
 
   private ImmutableList<ImmutableList<RexLiteral>> tupleList(int columnCount,
@@ -874,15 +892,38 @@ public class RelBuilder {
    * cannot, such as all values of a column being null, or there being zero
    * rows.
    *
-   * @param rowType Row type
    * @param tupleList Tuple list
+   * @param rowType Row type
    */
-  protected RelBuilder values(RelDataType rowType,
-      Iterable<ImmutableList<RexLiteral>> tupleList) {
-    RelNode values = valuesFactory.createValues(cluster, rowType,
-        ImmutableList.copyOf(tupleList));
+  public RelBuilder values(Iterable<? extends List<RexLiteral>> tupleList,
+      RelDataType rowType) {
+    RelNode values =
+        valuesFactory.createValues(cluster, rowType, copy(tupleList));
     push(values);
     return this;
+  }
+
+  /** Converts an iterable of lists into an immutable list of immutable lists
+   * with the same contents. Returns the same object if possible. */
+  private static <E> ImmutableList<ImmutableList<E>>
+  copy(Iterable<? extends List<E>> tupleList) {
+    final ImmutableList.Builder<ImmutableList<E>> builder =
+        ImmutableList.builder();
+    int changeCount = 0;
+    for (List<E> literals : tupleList) {
+      final ImmutableList<E> literals2 =
+          ImmutableList.copyOf(literals);
+      builder.add(literals2);
+      if (literals != literals2) {
+        ++changeCount;
+      }
+    }
+    if (changeCount == 0) {
+      // don't make a copy if we don't have to
+      //noinspection unchecked
+      return (ImmutableList<ImmutableList<E>>) tupleList;
+    }
+    return builder.build();
   }
 
   /** Creates a limit without a sort. */
@@ -1007,6 +1048,20 @@ public class RelBuilder {
     }
   }
 
+  /** Clears the stack.
+   *
+   * <p>The builder's state is now the same as when it was created. */
+  public void clear() {
+    stack.clear();
+  }
+
+  protected String getAlias() {
+    final Frame frame = Stacks.peek(stack);
+    return frame.right.size() == 1
+        ? frame.right.get(0).left
+        : null;
+  }
+
   /** Information necessary to create a call to an aggregate function.
    *
    * @see RelBuilder#aggregateCall */
@@ -1017,14 +1072,30 @@ public class RelBuilder {
    *
    * @see RelBuilder#groupKey */
   public interface GroupKey {
+    /** Assigns an alias to this group key.
+     *
+     * <p>Used to assign field names in the {@code group} operation. */
+    GroupKey alias(String alias);
   }
 
   /** Implementation of {@link RelBuilder.GroupKey}. */
-  private static class GroupKeyImpl implements GroupKey {
-    private final ImmutableList<RexNode> nodes;
+  protected static class GroupKeyImpl implements GroupKey {
+    final ImmutableList<RexNode> nodes;
+    final String alias;
 
-    GroupKeyImpl(ImmutableList<RexNode> nodes) {
+    GroupKeyImpl(ImmutableList<RexNode> nodes, String alias) {
       this.nodes = nodes;
+      this.alias = alias;
+    }
+
+    @Override public String toString() {
+      return alias == null ? nodes.toString() : nodes + " as " + alias;
+    }
+
+    public GroupKey alias(String alias) {
+      return Objects.equals(this.alias, alias)
+          ? this
+          : new GroupKeyImpl(nodes, alias);
     }
   }
 
